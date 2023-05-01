07cd711e9c695e0d2e25a7ea41d274e285442de9
Razmik Achikyan <razmikachikyan5@gmail.com>
2023-05-01 15:37:30 +0400

    lang-logic

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+import hayeren from "../lang/am.json" assert { type: "json" };
+import ruseren from "../lang/ru.json" assert { type: "json" };
+import angleren from "../lang/en.json" assert { type: "json" };
+
+console.log(hayeren);
+
+console.log(ruseren);
+
+console.log(angleren);
